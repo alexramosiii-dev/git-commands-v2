83a7ebadb22a096572607cc6dafb89724555c8df
alexramosiii-dev <alexramosiii.dev@gmail.com>
2024-06-22 11:30:25 +0800

    feature/made add * work

--- models/git-command.js
+++ models/git-command.js
@@ -12,6 +12,17 @@ class GitCommand {
     //Command: git status
     // Paste the codes you already did from the "Git Out Bug!" assignment
     // status(){}
+    status(){
+        let files = this.working_directory.new_changes;
+        let message = `You have ${Object.keys(files).length} change/s.\n`;
+
+        for (let i = 0; i < Object.keys(files).length; i++) {
+            if(i == Object.keys(files).length - 1) message += `${Object.keys(files)[i]}`
+            else message += `${Object.keys(files)[i]}\n`;
+        }
+        
+        return message;
+    }
 
     //Command: git add <filename/file directory/wildcard> 
     add(path_file){
@@ -24,6 +35,15 @@ class GitCommand {
         /*
             Create logic here then run unit testing. Make sure that they all pass before sending PR.
         */
+        else if (path_file === "*") {
+            for (let file of Object.keys(modified_files)) {
+                if(file.startsWith(".github/")) {
+                    this.staging.push(modified_files[file]);
+                }else {
+                    delete modified_files[file];
+                }
+            }
+        }
         else{
             return `Failed to add ${path_file}! File is not modified or missing.`;
         }
